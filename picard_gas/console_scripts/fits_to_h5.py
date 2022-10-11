1c4f2c7ab6878db5998e27adf6ef11081e2fb209
Stefan Lepperdinger <lepperdinger.stefan@gmail.com>
2022-10-11 15:31:22 +0200

    fits_to_h5 is now also able to convert the HI gas.

--- picard_gas/console_scripts/fits_to_h5.py
+++ picard_gas/console_scripts/fits_to_h5.py
@@ -6,9 +6,6 @@ Author: Stefan Lepperdinger
 """
 from astropy.io import fits
 from picard_gas.H5File import H5File
-from picard_gas.initial_grid import GRID_VOLUME_LIMITS
-from picard_gas.initial_grid import GRID_CELL_CENTER_LIMITS
-from picard_gas.initial_grid import GRID_SHAPE
 import numpy as np
 import argparse
 import sys
@@ -17,14 +14,35 @@ import sys
 def read_fits_file(fits_file_path):
     try:
         with fits.open(fits_file_path) as header_data_unit_list:
+            header = header_data_unit_list[0].header
+            # x, y, and z limits of the volume represented by the grid
+            grid_volume_limits = []
+            # x, y, and z limits of the cell centers
+            grid_cell_center_limits = []
+            for coordinate in range(1, 4):  # 1, 2, 3 = x, y, z
+                first_cell_center = header[f'CRVAL{coordinate}']
+                cell_width = header[f'CDELT{coordinate}']
+                lower_volume_limit = first_cell_center - cell_width/2
+                assert lower_volume_limit < 0
+                # I assume the distribution is centered at the Galactic Center
+                upper_volume_limit = -lower_volume_limit
+                upper_cell_center = upper_volume_limit - cell_width/2
+                grid_volume_limits.append([
+                    lower_volume_limit,
+                    upper_volume_limit,
+                ])
+                grid_cell_center_limits.append([
+                    first_cell_center,
+                    upper_cell_center,
+                ])
+            grid_volume_limits = np.array(grid_volume_limits)
+            grid_cell_center_limits = np.array(grid_cell_center_limits)
             data = header_data_unit_list[0].data
     except FileNotFoundError:
         print(f"File '{fits_file_path}' not found.", file=sys.stderr)
         sys.exit(1)
 
-    assert np.all(data.shape == GRID_SHAPE), 'invalid grid shape'
-
-    return data
+    return data, grid_volume_limits, grid_cell_center_limits
 
 
 def parse_arguments():
@@ -48,10 +66,12 @@ def parse_arguments():
 
 def main():
     arguments = parse_arguments()
-    density = read_fits_file(arguments.source_path)
+    density, grid_volume_limits, grid_cell_center_limits = read_fits_file(
+        arguments.source_path
+    )
     h5_file = H5File(arguments.destination_path, 'w')
     h5_file.write_density(density)
-    h5_file.write_grid_limits(GRID_VOLUME_LIMITS, GRID_CELL_CENTER_LIMITS)
+    h5_file.write_grid_limits(grid_volume_limits, grid_cell_center_limits)
 
 
 if __name__ == '__main__':
